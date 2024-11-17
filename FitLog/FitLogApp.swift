851566b1906b4c1b79cd3446bdd3a437521937cf
HFMIRE <huda.f.mire@gmail.com>
2024-11-17 22:44:02 +0000

    Initial Commit

---
+++ FitLog/FitLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitLogApp.swift
+//  FitLog
+//
+//  Created by Hoda Mire on 17/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitLogTests/FitLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitLogTests.swift
+//  FitLogTests
+//
+//  Created by Hoda Mire on 17/11/2024.
+//
+
+import Testing
+@testable import FitLog
+
+struct FitLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
